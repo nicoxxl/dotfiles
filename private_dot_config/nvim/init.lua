49ffcabef0b656b0e6b88245384ea376c037a48d
Nicolas B <nicoxxl@laposte.net>
2024-03-04 17:40:02 +0100

    add nvim

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,45 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+ {
+   'nvim-telescope/telescope.nvim',
+   dependencies = { 'nvim-lua/plenary.nvim' }
+ }
+}, {})
+
+vim.wo.number = true
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
+
+require('telescope').setup {}
+pcall(require('telescope').load_extension, 'fzf')
+
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files)
+
+
+-- vim: ts=2 sts=2 sw=2 et
