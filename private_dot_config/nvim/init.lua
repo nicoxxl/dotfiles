128393259e80dcd5be58647fad7aab49aea127f1
Nicolas B. <nicolas.boudet@caramail.fr>
2024-04-18 21:53:50 +0200

    add nvim relative line number

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -27,6 +27,7 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
+vim.wo.relativenumber = true
 
 vim.cmd.colorscheme "catppuccin"
 
